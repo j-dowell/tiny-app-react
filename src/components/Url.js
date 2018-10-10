230bb4584353257e2a8751d22073bad2c78a052e
James Dowell <james.s.dowell@gmail.com>
2018-10-09 23:52:03 -0700

    added url component

---
+++ src/components/Url.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Url = ({ name, url, onDelete }) => {
+  <tr>
+    <td>
+      {url}
+    </td>
+    <td>
+      {name}
+    </td>
+    <td>
+      <a href={url} target='_blank'>
+      <button>Visit</button></a>
+    </td>
+    <td>
+      <button onClick={onDelete}>Delete</button>
+    </td>
+  </tr>
+}
+
+Url.propTypes = {
+  name: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+  onDelete: PropTypes.func.isRequired,
+}

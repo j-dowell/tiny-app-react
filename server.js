4b8c704439e26260aaf915188d4d9db3cb8d893a
James Dowell <james.s.dowell@gmail.com>
2018-10-09 23:07:26 -0700

    added react router

---
+++ server.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+const ENV = process.env.ENV || "development";
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'))
+})
+
+const port = process.env.PORT || 3005;
+app.listen(port);
+
+console.log('App is listening on port ' + port);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'
+import { render } from 'react-dom'
+import { createStore } from 'redux'
+import todoApp from './reducers'
+import Root from './components/Root'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(todoApp)
+​
+render(
+  <Root store={store} />,
+  document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

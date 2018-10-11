cf526706f8a6c4982405fc065f203b1b6bd9d834
James Dowell <james.s.dowell@gmail.com>
2018-10-11 13:21:44 -0700

    linked mongo up to user sign in - checking for user email in db

--- server.js
+++ server.js
@@ -7,11 +7,28 @@ const jwt = require('jsonwebtoken')
 const key = process.env.JWT_key;
 const mongoUser = process.env.MONGO_USER;
 const mongoPw = process.env.MONGO_PW
-var MongoClient = require('mongodb').MongoClient;
+var MongoClient = require('mongodb').MongoClient,
+  co = require('co'),
+  assert = require('assert');
+  const uri = `mongodb+srv://${mongoUser}:${mongoPw}@cluster0-btgde.mongodb.net/test?retryWrites=true`;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-const uri = `mongodb+srv://${mongoUser}:${mongoPw}@cluster0-btgde.mongodb.net/test?retryWrites=true`;
+
+// co.wrap(function*() {
+//   // Connection URL
+//   var db = yield MongoClient.connect(uri, { useNewUrlParser: true });
+//   console.log("Connected correctly to server");
+
+//   // Get the collection
+//   var col = db.db('tiny-app').collection('users');
+//   var docs = yield col.find().toArray();
+//   console.log(docs);
+
+// }).catch(function(err) {
+//   console.log(err.stack);
+// });
+
 
 app.get(`/token/:id`, (req, res) => {
   console.log('api token')
@@ -33,36 +50,45 @@ app.get(`/token/:id`, (req, res) => {
 app.post('/api/login', (req, res) => {
   console.log('making login request')
   console.log('req body', req.body)
-  const user = {
-    id: 1,
-    email: 'test@gmail.com',
-    password: 'test'
-  }
-  if (req.body.email === user.email && req.body.password === user.password) {
-    var token = jwt.sign({ id: user.id }, key, {
-      expiresIn: 86400 // expires in 24 hours
-    });
-    console.log(token)
-    res.json({auth:true, token:token})
-  } else {
-    res.json({auth:false})
-  }
+  co(function*() {
+    // Connection URL
+    let db = yield MongoClient.connect(uri, { useNewUrlParser: true });
+    console.log("Connected correctly to server");
+  
+    // Get the collection
+    let col = db.db('tiny-app').collection('users');
+    // Search for user by email
+    let docs = yield col.find({email: req.body.email}).toArray();
+    const user = docs[0];
+    // Checking password
+    if (user.password_digest === req.body.password) {
+      const token = jwt.sign({ id: user._id }, key, {
+        expiresIn: 86400 // expires in 24 hours
+      });
+      console.log('Token:', token)
+      res.json({auth:true, token:token})
+    } else {
+      console.log('No token given, auth false')
+      res.json({auth:false})
+    }
+  }).catch(function(err) {
+    console.log(err.stack);
+  });
 });
 
-
-(async function() {
-  try {
-    const client = await MongoClient.connect(uri,{ useNewUrlParser: true });
-    const collection = client.db('tiny-app').collection('users')
-    collection.find({}).toArray(function(err, result) {
-    if (err) throw err;
-    console.log(result);
-    client.close();
-  });
-  } catch(e) {
-    console.error(e)
-  }
-})();
+// (async function() {
+//   try {
+//     const client = await MongoClient.connect(uri,{ useNewUrlParser: true });
+//     const collection = client.db('tiny-app').collection('users')
+//     collection.find({}).toArray(function(err, result) {
+//     if (err) throw err;
+//     console.log(result);
+//     client.close();
+//   });
+//   } catch(e) {
+//     console.error(e)
+//   }
+// })();
 
 
 const port = process.env.PORT || 3005;

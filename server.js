361964d7110ba3c9a1b31735dcbdcca2997833a6
James Dowell <james.s.dowell@gmail.com>
2018-10-11 15:15:50 -0700

    wired up mongodb search and bcrypt password compare

--- server.js
+++ server.js
@@ -2,32 +2,58 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 // const ENV = process.env.ENV || "development";
+
 const key = process.env.JWT_key;
 const mongoUser = process.env.MONGO_USER;
 const mongoPw = process.env.MONGO_PW
-var MongoClient = require('mongodb').MongoClient,
-  co = require('co'),
-  assert = require('assert');
-  const uri = `mongodb+srv://${mongoUser}:${mongoPw}@cluster0-btgde.mongodb.net/test?retryWrites=true`;
+const MongoClient = require('mongodb').MongoClient;
+const url = `mongodb+srv://${mongoUser}:${mongoPw}@cluster0-btgde.mongodb.net/test?retryWrites=true`;
+const dbName = 'tiny-app';
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+const saltRounds = 10;
 
-// co.wrap(function*() {
-//   // Connection URL
-//   var db = yield MongoClient.connect(uri, { useNewUrlParser: true });
-//   console.log("Connected correctly to server");
 
-//   // Get the collection
-//   var col = db.db('tiny-app').collection('users');
-//   var docs = yield col.find().toArray();
-//   console.log(docs);
+const verifyUserLogin = async function(email, password) {
+const client = new MongoClient(url, { useNewUrlParser: true });
+  try {
+    // Connecting to Mongo
+    await client
+      .connect()
+      .catch(err => console.log(err));
+    console.log("Connected correctly to server");
+    const db = client.db(dbName);
+    // Accessing users collection
+    const col = db.collection('users');
+    // Searching for user by email
+    const result = await col
+      .find({email: email})
+      .toArray()
+      .catch(err => console.log(err))
+    const user = result[0];
+    // Checking hashed password
+    const passwordisValid = await bcrypt
+      .compare(password, user.password_digest)
+      .catch(err => console.log(err))
 
-// }).catch(function(err) {
-//   console.log(err.stack);
-// });
+    if (passwordisValid) {
+      return {
+        id: user._id,
+        auth: true
+      }
+    } else {
+      return {
+        auth: false
+      }
+    }
+  } catch(err) {
+    console.log(err.stack);
+  }
+}
 
 
 app.get(`/token/:id`, (req, res) => {
@@ -50,19 +76,11 @@ app.get(`/token/:id`, (req, res) => {
 app.post('/api/login', (req, res) => {
   console.log('making login request')
   console.log('req body', req.body)
-  co(function*() {
-    // Connection URL
-    let db = yield MongoClient.connect(uri, { useNewUrlParser: true });
-    console.log("Connected correctly to server");
-  
-    // Get the collection
-    let col = db.db('tiny-app').collection('users');
-    // Search for user by email
-    let docs = yield col.find({email: req.body.email}).toArray();
-    const user = docs[0];
-    // Checking password
-    if (user.password_digest === req.body.password) {
-      const token = jwt.sign({ id: user._id }, key, {
+
+  verifyUserLogin(req.body.email, req.body.password).then((result) => {
+    console.log('verification result', result)
+    if (result.auth === true) {
+      const token = jwt.sign({ id: result.id }, key, {
         expiresIn: 86400 // expires in 24 hours
       });
       console.log('Token:', token)
@@ -71,24 +89,9 @@ app.post('/api/login', (req, res) => {
       console.log('No token given, auth false')
       res.json({auth:false})
     }
-  }).catch(function(err) {
-    console.log(err.stack);
-  });
+  })
+  .catch(err => console.log(err));
 });
-
-// (async function() {
-//   try {
-//     const client = await MongoClient.connect(uri,{ useNewUrlParser: true });
-//     const collection = client.db('tiny-app').collection('users')
-//     collection.find({}).toArray(function(err, result) {
-//     if (err) throw err;
-//     console.log(result);
-//     client.close();
-//   });
-//   } catch(e) {
-//     console.error(e)
-//   }
-// })();
 
 
 const port = process.env.PORT || 3005;

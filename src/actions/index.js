f33f4d315d5ef4aa34c55454e27c0515d73ef9e9
James Dowell <james.s.dowell@gmail.com>
2018-10-11 16:35:38 -0700

    added register user action

--- src/actions/index.js
+++ src/actions/index.js
@@ -17,30 +17,6 @@ export const AUTHENTICATED = 'authenticated_user';
 export const UNAUTHENTICATED = 'unauthenticated_user';
 export const AUTHENTICATION_ERROR = 'authentication_error';
 
-// export function signInAction({ email, password }, history) {
-//   return async (dispatch) => {
-//     try {
-//       const res = await axios.post(`/api/login`, { email, password });
-//       console.log(res);
-//       if (res.data !== 'true') {
-//         dispatch({
-//           type: AUTHENTICATION_ERROR,
-//           payload: 'Invalid email or password'
-//         });
-//       } else {
-//         dispatch({ type: AUTHENTICATED });
-//         localStorage.setItem('user', res.data);
-//         console.log(localStorage)
-//         history.push('/');
-//       }
-//     } catch(error) {
-//       dispatch({
-//         type: AUTHENTICATION_ERROR,
-//         payload: 'Invalid email or password'
-//       });
-//     }
-//   };
-// }
 export function signInAction({ email, password }, history) {
 
   return function (dispatch) {
@@ -90,3 +66,11 @@ export function signOutAction() {
     type: UNAUTHENTICATED
   }
 }
+
+export const registerUser = (email, password, first_name, last_name) => ({
+  type: 'REGISTER_USER',
+  email,
+  password,
+  first_name,
+  last_name
+})

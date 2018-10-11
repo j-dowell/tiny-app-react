f377c60aa75ce65658ffb2d3481ee0efeb3458a2
James Dowell <james.s.dowell@gmail.com>
2018-10-11 11:14:55 -0700

    changed redirection if logged in

--- src/components/hoc/no_require_auth.js
+++ src/components/hoc/no_require_auth.js
@@ -6,13 +6,13 @@ export default function (ComposedComponent) {
   class NotAuthentication extends Component {
     componentWillMount() {
       if (this.props.authenticated) {
-        this.props.history.push('/secret');
+        this.props.history.push('/urls');
       }
     }
 
     componentWillUpdate(nextProps) {
       if (nextProps.authenticated) {
-        this.props.history.push('/secret');
+        this.props.history.push('/urls');
       }
     }
 

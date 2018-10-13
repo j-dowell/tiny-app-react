a920f0bf313e5606bef04d90f9a12dde9cd1202a
James Dowell <james.s.dowell@gmail.com>
2018-10-13 14:40:45 -0700

    showing navbar on each page

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import AddNewUrl from './Add';
 import UrlListContainer from '../containers/UrlListContainer';
-
+import NavBar from './NavBar';
 class App extends Component {
   render() {
     return (
       <div>
         <p>TinyURL</p>
+        <NavBar/>
         <AddNewUrl/>
         <UrlListContainer/>
       </div>

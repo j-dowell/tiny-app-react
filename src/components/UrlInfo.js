f9537f42b83e20f5039e78e189a66de2ec2759e9
James Dowell <james.s.dowell@gmail.com>
2018-10-14 00:05:15 -0700

    changed text to material typography on url info page

--- src/components/UrlInfo.js
+++ src/components/UrlInfo.js
@@ -2,21 +2,22 @@ import React from 'react'
 // import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 // import {deleteUrl} from '../actions'
+import Typography from '@material-ui/core/Typography'
 
 const UrlInfo = ({ url }) => (
   <div>
   {url ? ( 
   <div>
     <div>
-      <h1>{url.name}</h1>
-      <h3>{url.url}</h3>
-      <h3>Created {new Date(url.date_added).toLocaleString()}</h3>
-      <h3>/{url.shortURL}</h3>
+      <Typography variant="h2">{url.name}</Typography>
+      <Typography variant="h3">{url.url}</Typography>
+      <Typography variant="h3">Created {new Date(url.date_added).toLocaleString()}</Typography>
+      <Typography variant="h3">/{url.shortURL}</Typography>
       <button>Copy</button><button>Share</button><button>Edit</button><button>Delete</button>
     </div>
     <div>
-      <p>Times clicked:</p>
-      <p>Locations</p>
+      <Typography>Times clicked:</Typography>
+      <Typography>Locations</Typography>
     </div>
   </div>) : (<p>click</p>)}
   </div>
